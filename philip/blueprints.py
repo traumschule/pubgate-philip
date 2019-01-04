bf7da248f99db9fa8918268f8e10700817699f08
mk <nozdre@gmail.com>
2019-01-05 01:18:50 +0200

    strip tags

--- philip/blueprints.py
+++ philip/blueprints.py
@@ -1,4 +1,5 @@
 import os
+from html.parser import HTMLParser
 
 from sanic import response, Blueprint
 from jinja2 import Environment, PackageLoader
@@ -16,6 +17,25 @@ jinja_env = Environment(
 )
 
 
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
 @philip_v1.route('/', methods=['GET'])
 async def home(request, **kwargs):
     data = await Outbox.find(filter={
@@ -24,6 +44,9 @@ async def home(request, **kwargs):
             },
             sort="activity.published desc")
     posts = Outbox.activity_clean(data.objects)
+    for post in posts:
+        post["object"]["content"] = strip_tags(post["object"]["content"])
+
     return response.html(
         # html_minify(
             jinja_env.get_template("home.jinja").render(
